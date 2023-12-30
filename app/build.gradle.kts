1ccaa961f642c5f1969efb567c6f4d4887c0a8b8
MubarakBasha <enpisb251@gmail.com>
2023-12-30 13:36:20 +0530

    Package's are structured for android architecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") // ksp
+    id("com.google.dagger.hilt.android") // dagger-hilt
+    id("androidx.navigation.safeargs.kotlin") // safe-args
+    id("kotlin-parcelize") // parcelize transferring data between classes
 }
 
 android {
@@ -40,11 +44,50 @@ android {
 
 dependencies {
 
+    val roomVersion = "2.6.1"
+    val daggerVersion = "2.48"
+    val viewmodelVersion = "2.6.2"
+    val coroutineVersion = "1.7.3"
+    val navVersion = "2.7.6"
+
+    // Android Core (Kotlin)
     implementation("androidx.core:core-ktx:1.12.0")
+
+    // Android Core (Ui)
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Hilt (Dependency Injection)
+    implementation("com.google.dagger:hilt-android:$daggerVersion")
+    ksp("com.google.dagger:hilt-android-compiler:$daggerVersion")
+
+    // Room (Local Database)
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-paging:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$viewmodelVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$viewmodelVersion")
+
+    // kotlin Ktx
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    // Coroutines (Asynchronous Task)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
+
+    // Example Instrumental Test
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val navVersion = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" // Ksp (Kotlin symbolic processor) for generating code from annotation for room and hilt,...
+    id("com.google.dagger.hilt.android") version "2.48" apply false // Hilt
 }
