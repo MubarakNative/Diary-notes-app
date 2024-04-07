0dd1e77c0a848d4ebabc83604a70f9353dfa8e29
Mubarak <enpisb251@gmail.com>
2024-04-07 22:24:09 +0530

    Added Local unit test dependencies and declared the test doubles for testing

---
+++ app/src/test/java/com/mubarak/madexample/data/repository/FakeNoteRepository.kt
@@ -0,0 +1,62 @@
+package com.mubarak.madexample.data.repository
+
+import com.mubarak.madexample.data.sources.local.model.Note
+import com.mubarak.madexample.utils.NoteStatus
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/** FakeNoteRepository for testing*/
+class FakeNoteRepository : NoteRepository {
+
+    private val noteList = mutableListOf<Note>()
+    override suspend fun insertNote(note: Note) {
+        noteList.add(note)
+    }
+
+    override suspend fun upsertNote(note: Note) {
+        val index = noteList.indexOfFirst { it.id == note.id }
+        if (index != -1) {
+            noteList[index] = note
+        } else {
+            noteList.add(note)
+        }
+    }
+
+    override fun getAllNote(): Flow<List<Note>> {
+        return flow { emit(noteList) }
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        noteList.remove(note)
+    }
+
+    override suspend fun deleteNoteById(noteId: Long) {
+        noteList.removeAll { it.id == noteId }
+    }
+
+    override suspend fun deleteAllNotes() {
+        noteList.clear()
+    }
+
+    override fun searchNote(searchQuery: String): Flow<List<Note>> {
+        return flow {
+            val filter = noteList.filter { it.title == searchQuery || it.description == searchQuery }
+            emit(filter)
+        }
+    }
+
+    override fun getNoteStreamById(noteId: Long): Flow<Note> {
+        return flow {noteList.find { it.id == noteId }!! }
+    }
+
+    override fun getNoteByStatus(noteStatus: NoteStatus): Flow<List<Note>> {
+        return  flow {
+            val filter = noteList.filter { it.noteStatus == noteStatus}
+            emit(filter)
+        }
+    }
+
+    override suspend fun getNoteById(noteId: Long): Note {
+        return noteList.find { it.id == noteId }!!
+    }
+}
---
+++ app/src/test/java/com/mubarak/madexample/data/sources/datastore/FakeUserPreference.kt
@@ -0,0 +1,17 @@
+package com.mubarak.madexample.data.sources.datastore
+
+import com.mubarak.madexample.utils.NoteLayout
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/** FakeUserPreference for testing*/
+class FakeUserPreference : UserPreference {
+
+    private var noteLayout: String = NoteLayout.LIST.name
+    override suspend fun setNoteLayout(noteLayout: String) {
+        this.noteLayout = noteLayout
+    }
+
+    override val getNoteLayout: Flow<String>
+        get() = flow { noteLayout }
+}
